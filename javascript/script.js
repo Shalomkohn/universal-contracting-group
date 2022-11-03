f4ac0d1281fbf2097d0e327fa94a45c6d74faa20
Shalomkohn2 <sbk5775sbk5775@gmail.com>
2022-11-02 22:28:40 -0400

    Usable version 11-22

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,16 @@
-window.onscroll = ()=> scrollfunc(document.documentElement.scrollTop);
+
+window.onscroll = ()=> {
+    navbarEffectFunc(document.documentElement.scrollTop);
+    handleScrollAnimation()
+};
+
+
+// **********************************************************    Navbar Effects     *************************************************************************
 
 let lastPosition = 0
 let positionWhenScrolledUp = null;
 
-const scrollfunc = (newPosition) => {
+const navbarEffectFunc = (newPosition) => {
 
     let scrolledUp = newPosition > lastPosition;
     if ( scrolledUp && newPosition > positionWhenScrolledUp + 120) {
@@ -16,8 +23,53 @@ const scrollfunc = (newPosition) => {
 
     if(newPosition < 20) {
         document.getElementById("navbar").style.backgroundColor = "transparent";
+        document.getElementById("navbar").style.boxShadow = " 0px 0px 0px";
+
     } else {
-        document.getElementById("navbar").style.backgroundColor = "rgba(255,255,255, 0.7";
+        document.getElementById("navbar").style.backgroundColor = "rgba(255,255,255, 0.7)";
+        document.getElementById("navbar").style.boxShadow = " 0px 0px 5px rgba(200,200,200, 0.7)";
     }
     lastPosition = document.documentElement.scrollTop;
 }
+
+// *********************************************************    Scroll Effects     *************************************************************************
+
+const scrollEffectElements = document.querySelectorAll(".js-scroll-effect");
+
+const isElementInView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return elementTop < ((window.innerHeight || document.documentElement.clientHeight) - 150)
+}
+
+scrollEffectElements.forEach((el) => {
+    el.classList.remove("scrolled");
+})
+
+
+const displayScrollElement = (el) => {
+    el.classList.add("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollEffectElements.forEach((el) => {
+        if(isElementInView(el)){
+            displayScrollElement(el)
+        } else {
+            el.classList.remove("scrolled");
+        }
+    })
+}
+
+// *********************************************************    On Click Styling     *************************************************************************
+
+let projectBoxes = document.querySelectorAll(".project");
+
+document.addEventListener("click", (e) => {
+    projectBoxes.forEach((projectBox) => {
+        if(!projectBox.contains(e.target)){
+            projectBox.classList.remove("active")
+        } else {
+            projectBox.classList.add("active")
+        }
+    })
+})
